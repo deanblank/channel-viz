83632b10f59fbbba52563e76ade2855b511f2531
deanblank <mail@deanblank.net>
2013-07-13 14:00:26 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'dabShieldMaster', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1530228271], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= '', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'dabShield', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
